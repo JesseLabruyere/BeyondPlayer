d786ad2da80d8e9ec1fb6bce79dc447c6a29eb22
JesseLabruyere <JesseLabruyere@gmail.com>
2015-07-22 16:58:27 +0200

    fileupload html ingevoegd

--- app/Resources/scripts/test2.js
+++ app/Resources/scripts/test2.js
@@ -14,9 +14,25 @@ function assignEventHandlers() {
     $('#leftbar_upload').click(function() {
         loadUploadPage();
     });
+
+    /* onchange for the fileInput*/
+    $( document).on( "change", "#fileInput", function() {
+        uploadFile(this);
+    });
 }
 
 /*function that loads the uploadpage*/
 function loadUploadPage() {
 
+}
+
+function uploadFile(fileInput){
+    if ('files' in fileInput) {
+        var file;
+        for(var i = 0; i < fileInput.files.length; i++) {
+            file = fileInput.files[i];
+            alert(file.name + " - " + file.size + " bytes");
+        }
+
+    }
 }
