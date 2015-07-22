9d6c0f11f6eacb3cc6bec509045548f104025dfe
JesseLabruyere <JesseLabruyere@gmail.com>
2015-07-22 11:56:42 +0200

    directive voor onclick uitgetest

--- app/Resources/scripts/test1.js
+++ app/Resources/scripts/test1.js
@@ -4,10 +4,15 @@
 var app = angular.module('app',['ngSanitize']);
 
 /* mainly used for editting the models of elements*/
+/*
 app.controller('MyController', ['$scope', '$http', function($scope, $http) {
-    /*creating a new object called functions*/
+    */
+/*creating a new object called functions*//*
+
     $scope.functions = {};
-    /*adding new function called loadUploadPage to the 'functions' object*/
+    */
+/*adding new function called loadUploadPage to the 'functions' object*//*
+
     $scope.functions.loadUploadPage = function(item, event) {
 
         var response = $http.get("app/uploadForm");
@@ -21,27 +26,43 @@ app.controller('MyController', ['$scope', '$http', function($scope, $http) {
         });
     }
 }]);
+*/
 
 /* all real dom manipulation should be done in directives*/
-/*app.directive('MyDirective', ['$scope', '$http', function($scope, $http) {
-    *//*creating a new object called functions*//*
-    $scope.functions = {};
-    *//*adding new function called loadUploadPage to the 'functions' object*//*
-    $scope.functions.loadUploadPage = function(item, event) {
+app.directive('leftMenuDirective', function() {
 
-        var response = $http.get("app/uploadForm");
+    // we link onclick
+    var linkFn = function ($scope, $http) {
 
-        response.success(function(data, status, headers, config) {
-            alert($(data).prop('outerHTML'));
-            $scope.pageCenter  = 'test <input type="file" title="Search for a file to add"> test' +
-            '<a href="#">links!</a> and other <em>stuff</em>';
-            *//*$('input[type=file]').bootstrapFileInput();*//*
-        });
-        response.error(function(data, status, headers, config) {
-            alert("AJAX failed!");
-        });
-    }
-}]);*/
+        /*creating a new object called functions*/
+        $scope.functions = {};
+
+        /*adding new function called loadUploadPage to the 'functions' object*/
+        $scope.functions.loadUploadPage = function (item, event) {
+            /*JSON call*/
+            var response = $http.get("app/uploadForm");
+            response.success(function (data, status, headers, config) {
+                $('#pageCenter').html(data);
+                $('input[type=file]').bootstrapFileInput();
+            });
+            response.error(function (data, status, headers, config) {
+                alert("AJAX failed!");
+            });
+        };
+
+        /*adding new function called loadListenPage to the 'functions' object*/
+        $scope.functions.loadListenPage = function (item, event) {
+            $('#pageCenter').html('<h1>Listenpage</h1>');
+        };
+
+    };
+
+    return {
+        /*'A' can be used if we use the directive as an attribute*/
+        restrict: 'E',
+        controller: linkFn
+    };
+});
 
 
 
